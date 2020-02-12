9fcfde7ee532e8254e4e427fac48cf79d37cbc89
njagatap <naresh19@gmail.com>
2020-02-12 17:22:27 +0530

    printing port

--- SampleService/src/main/java/com/wallnet/service/controller/SampleResource.java
+++ SampleService/src/main/java/com/wallnet/service/controller/SampleResource.java
@@ -1,5 +1,7 @@
 package com.wallnet.service.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/sample")
 public class SampleResource {
 
+	@Autowired
+	private Environment env;
+	
 	@RequestMapping("/getstring")
 	public String get() {
-		return "Hello world";
+		return "Hello world from port" + env.getProperty("local.server.port");
 	}
 }
